cf9d562674f5028d140ec283f36f856eb28fcf1b
oskarrr991 <oskarrr991@gmail.com>
2019-06-14 10:53:08 +0300

    Login first commit

--- Fighting-game/src/app/app.module.ts
+++ Fighting-game/src/app/app.module.ts
@@ -1,24 +1,37 @@
+import { AuthService } from './auth/auth.service.service';
+import { AngularFireModule } from '@angular/fire';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { LoginComponent } from './admin/login/login.component';
+import { HeaderComponent } from './header/header.component';
 
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
-import { AngularFirestore } from '@angular/fire/firestore';
+const config = {
+  apiKey: 'AIzaSyDGo7ZbbSGzja22l2yQi3ovJo971mYWg5w',
+  authDomain: 'first-project-20ab3.firebaseapp.com',
+  databaseURL: 'https://first-project-20ab3.firebaseio.com',
+  projectId: 'first-project-20ab3',
+  storageBucket: 'first-project-20ab3.appspot.com',
+  messagingSenderId: '1016833167186',
+  appId: '1:1016833167186:web:c556ef1603552f1f'
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestore
+    AngularFireModule.initializeApp(config),
+    AngularFireAuthModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Fighting-game/src/environments/environment.ts
+++ Fighting-game/src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebaseConfig: {
-    apiKey: 'AIzaSyBG7tm5k4bd9SuClz4V5-GacV7rqjlsRSU',
-    authDomain: 'projektas-50cb2.firebaseapp.com',
-    databaseURL: 'https://projektas-50cb2.firebaseio.com',
-    projectId: 'projektas-50cb2',
-    storageBucket: 'projektas-50cb2.appspot.com',
-    messagingSenderId: '547805430882',
-    appId: '1:547805430882:web:0935b93d1298f791'
-  }
+  production: false
 };
 
 /*
