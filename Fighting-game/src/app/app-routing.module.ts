01c8f92970b4508caa002177147e68f56c02088c
John <equipus123@gmail.com>
2019-06-14 10:56:43 +0300

    Created main view

--- Fighting-game/src/app/app-routing.module.ts
+++ Fighting-game/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { MapComponent } from './map/map.component';
+import { NewsComponent } from './news/news.component';
+
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: 'header', component: HeaderComponent},
+{path: 'main', component: MainComponent},
+{path: 'map', component: MapComponent},
+{path: 'news', component: NewsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Fighting-game/src/app/app.module.ts
+++ Fighting-game/src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppComponent } from './app.component';
 
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { MapComponent } from './map/map.component';
+import { NewsComponent } from './news/news.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    MapComponent,
+    NewsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestore
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
