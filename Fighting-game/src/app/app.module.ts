9424fd14563960c1d01555007f6d285632c3541a
tomas19971010 <statkeviciust@gmail.com>
2019-06-17 10:23:29 +0300

    [F10]: added Form control and  validations.

--- Fighting-game/src/app/app.module.ts
+++ Fighting-game/src/app/app.module.ts
@@ -15,6 +15,28 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { SignUpComponent } from './components/sign-up/sign-up.component';
 import { CommonModule } from '@angular/common';
 
+import {FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule,
+  MatSelectModule,
+  MatButtonModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatSortModule,
+  MatDialogModule,
+  MatAutocompleteModule,
+  MatExpansionModule,
+  MatProgressSpinnerModule,
+  MatTabsModule,
+  MatSnackBarModule
+} from '@angular/material';
+
+
 
 
 @NgModule({
@@ -32,9 +54,16 @@ import { CommonModule } from '@angular/common';
     AngularFirestoreModule,
     AngularFireAuthModule,
     CommonModule,
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatCardModule,
+    MatInputModule
   ],
-  
+
   providers: [AuthService, LobbyService],
   bootstrap: [AppComponent]
 })
