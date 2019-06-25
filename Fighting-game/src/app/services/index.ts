7a6ae478f94b8d7771a89575be446cbc72cc4e1b
Simas <pasakaites@gmail.com>
2019-06-25 11:49:52 +0300

    [F2]: cosmetic changes.

--- Fighting-game/src/app/services/index.ts
+++ Fighting-game/src/app/services/index.ts
@@ -1,8 +1,10 @@
+import { FightService } from './fight.service';
 import { AuthService } from './auth.service';
 import { LobbyService } from './lobby.service';
 
 
 export {
     LobbyService,
-    AuthService
+    AuthService,
+    FightService
 };
