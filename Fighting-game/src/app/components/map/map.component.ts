29f2d09148efaf09dc92e5c07cf37af8909ea41a
ingavisinskaite <ingavisinskaite@gmail.com>
2019-06-26 12:56:47 +0300

    [F23] made map scrollable with mouse move

--- Fighting-game/src/app/components/map/map.component.ts
+++ Fighting-game/src/app/components/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
 
 @Component({
   selector: 'app-map',
@@ -7,15 +8,32 @@ import { Component, OnInit, HostListener } from '@angular/core';
 })
 export class MapComponent implements OnInit {
   toolTipPosition = 'above';
+  screenHeight: number;
+  screenWidth: number;
 
-  @HostListener('window:scroll', ['$event']) 
-  scrollHandler(event) {
-    console.log('Scroll Event');
-  }
+  constructor(private viewportScroller: ViewportScroller) {
+    this.getScreenSize();
+   }
 
-  constructor() { }
+  @HostListener('mousemove', ['$event'])
+  onMouseMove(e) {
+    let mousePosition = ((e.clientX / this.screenWidth) * 100);
+    this.scroll(e.clientX, e.clientY);
+}
+
+@HostListener('window:resize', ['$event'])
+getScreenSize(event?) {
+   this.screenHeight = screen.height;
+   this.screenWidth = screen.width;
+   console.log(this.screenHeight);
+   console.log(this.screenWidth);
+}
 
   ngOnInit() {
+  }
+ 
+  scroll(x, y) {
+    this.viewportScroller.scrollToPosition([x, y]);
   }
 
 }
