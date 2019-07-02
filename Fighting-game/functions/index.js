8bd6e6d8e6d8c45b332f80b99e396231a0bd0704
ingavisinskaite <ingavisinskaite@gmail.com>
2019-07-02 10:54:11 +0300

    [F3] added firebase function to send goodbye email when user deleted

--- Fighting-game/functions/index.js
+++ Fighting-game/functions/index.js
@@ -7,7 +7,7 @@ admin.initializeApp();
 
 var smtpTransport = nodemailer.createTransport("smtps://fightinggame8%40gmail.com:"+encodeURIComponent('f1ght1nggame') + "@smtp.gmail.com:465"); 
 
-exports.sendMailVerification = functions.auth.user().onCreate(user => {
+exports.sendWelcomeMail = functions.auth.user().onCreate(user => {
       
     const dest = user.email;
 
@@ -17,6 +17,26 @@ exports.sendMailVerification = functions.auth.user().onCreate(user => {
             subject: 'Welcome Email',
             html: `<h1>Welcome to our fighting game!</h1> <br>
                     <h3>We hope you will enjoy it.</h3>`
+        };
+        
+        return smtpTransport.sendMail(mailOptions, (error, info) => {
+            if(error){
+                console.log(error.toString());
+            }
+            console.log('Sended');
+        }); 
+});
+
+exports.sendGoodbyeMail = functions.auth.user().onDelete(user => {
+      
+    const dest = user.email;
+
+        const mailOptions = {
+            from: 'fightinggame8@gmail.com',
+            to: dest,
+            subject: 'Goodbye',
+            html: `<h1>We're sorry to see you go!</h1> <br>
+                    <h3>We hope you enjoyed it.</h3>`
         };
         
         return smtpTransport.sendMail(mailOptions, (error, info) => {
